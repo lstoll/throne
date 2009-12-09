c0eba3f64f9fabb42fc44b1ed1bcb5646743eebf
Lincoln Stoll <lstoll@lstoll.net>
2009-12-09 17:02:15 +1100

    Adding database create/delete functions

--- lib/smeg2/database.rb
+++ lib/smeg2/database.rb
@@ -1,4 +1,34 @@
 # Represents a connection to the database. Can be used standalone to talk
 # to a couch instance manually, otherwise is used by the mapper.
 class Smeg2::Database
+  # Create a new instance, with the URL for the database to work with
+  def initialize(url)
+    @url = url
+  end
+
+  def create
+    begin
+      c.put @url, {}
+    rescue RestClient::RequestFailed => e
+      unless e.message =~ /412$/
+        raise e
+      end
+    end
+  end
+
+  def delete!
+    begin
+      c.delete @url
+    rescue RestClient::ResourceNotFound
+    end
+  end
+
+  def create!
+    delete!
+    create
+  end
+
+  private
+
+  def c; RestClient; end
 end
--- spec/database_spec.rb
+++ spec/database_spec.rb
@@ -1,7 +1,44 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe Smeg2::Database do
-  it "should be able to create a database"
+  before :all do
+    @dburl =  'http://localhost:5984/smeg2_test'
+    begin
+      RestClient.delete(@dburl)
+    rescue
+    end
+    @db = Smeg2::Database.new(@dburl)
+  end
+
+  describe "creating/deleting databases" do
+    it "should be able to force delete/create a database" do
+      @db.create!
+      lambda {RestClient.get(@dburl)}.
+        should_not raise_error(RestClient::ResourceNotFound)
+    end
+
+    it "should be able to delete a database" do
+      @db.create!
+      @db.delete!
+      lambda {RestClient.get(@dburl)}.
+        should raise_error(RestClient::ResourceNotFound)
+    end
+
+    it "should be able to create a database" do
+      @db.delete!
+      @db.create
+      lambda {RestClient.get(@dburl)}.
+        should_not raise_error(RestClient::ResourceNotFound)
+    end
+
+    it "should not error when creating a database that exists" do
+      @db.delete!
+      @db.create
+      lambda {@db.create}.
+        should_not raise_error(Exception)
+    end
+  end
+
   it "should be able to get a doc by it's ID"
   it "should be able to post a new doc"
   it "should be able to update an existing doc"
