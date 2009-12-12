74c71b9017243dd011675deb05bd6426fa9c9a1a
Lincoln Stoll <lstoll@lstoll.net>
2009-12-12 20:23:44 +1100

    Basic database CRUD access.

--- lib/smeg2/database.rb
+++ lib/smeg2/database.rb
@@ -1,12 +1,18 @@
 # Represents a connection to the database. Can be used standalone to talk
 # to a couch instance manually, otherwise is used by the mapper.
 class Smeg2::Database
+  attr_reader :url
+
   # Create a new instance, with the URL for the database to work with
-  def initialize(url)
+  # By default will create the database if it doesn't exist, pass false
+  # as the second parameter to disable this.
+  def initialize(url, autocreate = true)
     @url = url
+    create_database if autocreate
   end
 
-  def create
+  # Creates this database, will not error if the database exists
+  def create_database
     begin
       c.put @url, {}
     rescue RestClient::RequestFailed => e
@@ -16,17 +22,44 @@ class Smeg2::Database
     end
   end
 
-  def delete!
+  # deletes this database.
+  def delete_database
     begin
       c.delete @url
     rescue RestClient::ResourceNotFound
     end
   end
 
-  def create!
-    delete!
-    create
+  # gets a document by it's ID
+  def get(docid)
+    JSON.parse(c.get(@url + '/' + docid))
   end
+
+  # creates/updates a document from a hash/array structure
+  def save(doc)
+    id = nil
+    if id = doc['_id']
+      c.put(@url + '/' + id, doc.to_json)
+    else
+      res = c.post(@url, doc.to_json)
+      res = JSON.parse(res)
+      id = res['id']
+    end
+    id
+  end
+
+  # deletes a document. Can take an object or id
+  def delete(doc)
+    if doc.kind_of? String
+      rev = get(doc)['_rev']
+    else
+      rev = doc['_rev']
+      doc = doc['_id']
+    end
+
+    c.delete(@url + '/' + doc + '?rev=' + rev)
+  end
+
 
   private
 
--- spec/database_spec.rb
+++ spec/database_spec.rb
@@ -8,39 +8,81 @@ describe Smeg2::Database do
     rescue
     end
     @db = Smeg2::Database.new(@dburl)
+    # some kind of fixtures loading?
   end
 
   describe "creating/deleting databases" do
-    it "should be able to force delete/create a database" do
-      @db.create!
-      lambda {RestClient.get(@dburl)}.
-        should_not raise_error(RestClient::ResourceNotFound)
-    end
-
     it "should be able to delete a database" do
-      @db.create!
-      @db.delete!
+      @db.create_database
+      @db.delete_database
       lambda {RestClient.get(@dburl)}.
         should raise_error(RestClient::ResourceNotFound)
     end
 
     it "should be able to create a database" do
-      @db.delete!
-      @db.create
+      @db.delete_database
+      @db.create_database
       lambda {RestClient.get(@dburl)}.
         should_not raise_error(RestClient::ResourceNotFound)
     end
 
     it "should not error when creating a database that exists" do
-      @db.delete!
-      @db.create
-      lambda {@db.create}.
+      @db.delete_database
+      @db.create_database
+      lambda {@db.create_database}.
         should_not raise_error(Exception)
+    end
+
+    it "shoudld automatically create the database" do
+      @db.delete_database
+      @db = Smeg2::Database.new(@dburl)
+      lambda {RestClient.get(@dburl)}.
+        should_not raise_error(RestClient::ResourceNotFound)
     end
   end
 
-  it "should be able to get a doc by it's ID"
-  it "should be able to post a new doc"
-  it "should be able to update an existing doc"
+  describe "working with documents" do
+    before :all do
+      @db = Smeg2::Database.new(@dburl)
+    end
+
+    it "should return a valid ID when creating" do
+      id = @db.save(:testfield => 'true')
+      id.should_not be_empty
+      lambda {RestClient.get(@dburl + '/' + id)}.
+        should_not raise_error(Exception)
+    end
+
+    it "should be able to get a doc by it's ID" do
+      id = @db.save(:testfield => 'true')
+      doc = @db.get(id)
+      doc.should_not be_nil
+      doc['testfield'].should_not be_nil
+    end
+
+    it "should be able to update an existing doc" do
+      id =  @db.save(:testfield => 'true')
+      doc = @db.get(id)
+      doc['testerfield'] = 'false'
+      @db.save(doc)
+      @db.get(id)['testerfield'].should eql('false')
+    end
+
+    it "should be able to delete a document when given a document" do
+      id = @db.save(:testfield => 'true')
+      doc = @db.get(id)
+      @db.delete(doc)
+      lambda {@db.get(id)}.
+        should raise_error(RestClient::ResourceNotFound)
+    end
+
+    it "should be able to delete a document when given a document id" do
+      id = @db.save(:testfield => 'true')
+      @db.delete(id)
+      lambda {@db.get(id)}.
+        should raise_error(RestClient::ResourceNotFound)
+    end
+  end
+
   it "should be able to get a view"
 end
