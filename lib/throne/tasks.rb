64d22e1aa3c1192a4baa0184cd7b9c19133e749a
Lincoln Stoll <lstoll@lstoll.net>
2009-12-16 22:35:48 +1100

    Totally redo the tasks data structure, to make it more better

--- lib/throne/tasks.rb
+++ lib/throne/tasks.rb
@@ -7,92 +7,107 @@ require 'yaml'
 # are stored in, if DB_URL is set that database is explicitly used, 
 # overriding the name of the folder/task. 
 class Throne::Tasks
-  # Injects rake tasks for loading and dumping data from databases.
-  # @param [String] base_path the path where design docs are stored
-  def self.inject_data_tasks(base_path)
-    namespace :throne do
-      namespace :load_data do
-        dirs_in_path(base_path).each do |dir, fulldir|
-          desc "Loads data for database #{dir}"
-          task dir.to_sym do
-            load_data_for_database(url(dir), fulldir)
-          end
-        end
-
-        desc "load all dbs"
-        task :all => dirs_in_path(base_path)
-      end
-      namespace :dump_data do
-        desc "Dump data for a database into a YAML file"
-        task :yml, :db do |t, args|
-          dump_docs_from_db(args.db, base_path, :yml)
-        end
-
-        desc "Dump data for a database into a JSON file"
-        task :json, :db do |t, args|
-          dump_docs_from_db(args.db, base_path, :json)
-        end
-      end
-    end
-  end
-
   # This will inject Rake tasks for loading design docs into the DB
   # The docs should be layed out in the following format:
+  #
   # base_path/
+  # |-- lib
+  # |    `-- library.js
+  # |-- data
+  # |   `-- .json and .yml folders with seed data
+  # |-- design
+  # |   |-- lists
+  # |   |   `-- statuses
+  # |   |       `-- list.js
+  # |   `-- views
+  # |       `-- statuses
+  # |           |-- map.js
+  # |           `-- reduce.js 
   # |-- <db name>
-  #      `-- <design doc name>
-  #          |-- lists
-  #          |   `-- statuses
-  #          |       `-- list.js
-  #          `-- views
-  #              `-- statuses
-  #                  |-- map.js
-  #                  `-- reduce.js 
+  #     |-- lib
+  #     |    `-- library.js
+  #     |-- data
+  #     |   `-- .json and .yml folders with seed data
+  #     `-- design
+  #          `-- <design doc name>
+  #              |-- lists
+  #              |   `-- statuses
+  #              |       `-- list.js
+  #              `-- views
+  #                  `-- statuses
+  #                      |-- map.js
+  #                      `-- reduce.js 
   #
   # @param [String] base_path the path where design docs are stored
-  def self.inject_design_doc_tasks(base_path)
+  def self.inject_tasks(base_path)
     namespace :throne do
-      namespace :push_design do
-        dirs_in_path(base_path).each do |dir, fulldir|
-          desc "pushes designs for database #{dir}"
-          task dir.to_sym do
-            load_design_documents(url(dir), fulldir)
+      namespace :documents do
+        namespace :load do
+          databases_in_path(base_path).each do |dbname, fulldir|
+            desc "Loads data for database #{dbname}, optionally into db"
+            task dbname.to_sym, :db do |t, args|
+              load_data_for_database(url(dbname, args), base_path, dbname)
+            end
+          end
+
+          desc "load all dbs"
+          task :all => databases_in_path(base_path)
+        end
+        namespace :dump do
+          desc "Dump data for a database into a YAML file"
+          task :yml, :db do |t, args|
+            dump_docs_from_db(args.db, base_path, :yml)
+          end
+
+          desc "Dump data for a database into a JSON file"
+          task :json, :db do |t, args|
+            dump_docs_from_db(args.db, base_path, :json)
           end
         end
-
-        desc "push all designs"
-        task :all => dirs_in_path(base_path)
-      end
-    end
-  end
-
-  # Injects tasks to create and delete databases
-  def self.inject_database_tasks(base_path)
-    namespace :throne do
-      desc "Creates a database if it doesn't exist"
-      task :createdb, :db do |t, args|
-        get_db(args).create_database
-        puts "Database at #{url(args.db)}"
       end
 
-      desc "Deletes a database"
-      task :deletedb, :db do |t, args|
-        get_db(args).delete_database
-        puts "Database at #{url(args.db)}"
+      #design tasks
+      namespace :design do
+        namespace :push do
+          databases_in_path(base_path).each do |dbname, fulldir|
+            desc "pushes designs for database #{dbname}, optionally into db"
+            task dbname.to_sym, :db do |t, args|
+              load_design_documents(url(dbname, args), base_path, dbname)
+            end
+          end
+
+          desc "push all designs"
+          task :all => databases_in_path(base_path)
+        end
       end
 
-      desc "Deletes, creates and re-loads a database"
-      task :rebuilddb, :db do |t, args|
-        db = get_db(args)
-        raise "you must specify the database name (task[db])" unless args.db
-        db.delete_database
-        # re-getting the object will create the DB
-        db = get_db(args)
-        # load design docs
-        load_design_documents(db.url, File.join(base_path, 'design', args.db))
-        # load data
-        load_data_for_database(db.url, File.join(base_path, 'data', args.db))
-        puts "Done."
+      # Db Tasks
+      namespace :database do
+        desc "Creates a database if it doesn't exist"
+        task :createdb, :db do |t, args|
+          get_db(args).create_database
+          puts "Database at #{url(args.db)}"
+        end
+
+        desc "Deletes a database"
+        task :deletedb, :db do |t, args|
+          get_db(args).delete_database
+          puts "Database at #{url(args.db)}"
+        end
+
+        desc "Deletes, creates and re-loads a database"
+        task :rebuilddb, :db do |t, args|
+          db = get_db(args)
+          raise "you must specify the database name (task[db])" unless args.db
+          db.delete_database
+          # re-getting the object will create the DB
+          db = get_db(args)
+          # load design docs
+          load_design_documents(db.url, File.join(base_path, 'design', args.db))
+          # load data
+          load_data_for_database(db.url, File.join(base_path, 'data', args.db))
+          puts "Done."
+        end
       end
 
 
@@ -118,44 +133,44 @@ class Throne::Tasks
 
   # Loads design documents into the database url, extracted from the source path
   # The docs should be layed out in the following format:
-  # base_path/
-  # |-- <db name>
-  #      `-- <design doc name>
-  #          |-- lists
-  #          |   `-- statuses
-  #          |       `-- list.js
-  #          `-- views
-  #              `-- statuses
-  #                  |-- map.js
-  #                  `-- reduce.js 
+  #  `-- <design doc name>
+  #      |-- lists
+  #      |   `-- statuses
+  #      |       `-- list.js
+  #      `-- views
+  #          `-- statuses
+  #              |-- map.js
+  #              `-- reduce.js 
   # @param [String] db_url the url of the database to load the data in to
   # @param [String] source_path the path to search for .yml and .json files
-  def self.load_design_documents(db_url, source_path)
+  def self.load_design_documents(db_url, base_path, database)
     # for each folder in base path, create a new design doc key
     # create a lists key
     # for each path in lists, add a key with the folder name
     # inside this, there is a key called list, with the contents of the list function
     # views is the same, except with a map and reduce function
-    Dir.glob(File.join(source_path, '*')).each do |doc_path|
-      next if File.basename(doc_path) == 'lib'
+    paths_for_item(base_path, database, 'design/*').each do |doc_path|
       doc_name = File.basename(doc_path)
       doc = {'lists' => {}, 'views' => {}}
+
       Dir.glob(File.join(doc_path, 'lists', '*')) do |list_path|
         list_name = File.basename(list_path)
         doc['lists'][list_name] = {}
         listfn = File.join(list_path, 'list.js')
         doc['lists'][list_name] = 
-            inject_code_includes(source_path, listfn) if File.exists?(listfn)
+            inject_code_includes(base_path, database, listfn) if File.exists?(listfn)
       end
+
       Dir.glob(File.join(doc_path, 'views', '*')) do |view_path|
         view_name = File.basename(view_path)
         doc['views'][view_name] = {}
         mapfn = File.join(view_path, 'map.js')
         reducefn = File.join(view_path, 'reduce.js')
         doc['views'][view_name]['map'] =
-            inject_code_includes(source_path, mapfn) if File.exists?(mapfn)
+            inject_code_includes(base_path, database, mapfn) if File.exists?(mapfn)
         doc['views'][view_name]['reduce'] =
-            inject_code_includes(source_path, reducefn) if File.exists?(reducefn)
+            inject_code_includes(base_path, database, reducefn) if 
+                File.exists?(reducefn)
       end
       # try to get the existing doc
       doc_id = "_design/#{doc_name}"
@@ -168,7 +183,7 @@ class Throne::Tasks
       end
       doc['language'] = 'javascript'
       db.save(doc)
-      puts "Design documents from #{source_path} loaded"
+      puts "Design documents from #{doc_path} loaded"
     end
     
     # try and get a document with the design name
@@ -181,16 +196,16 @@ class Throne::Tasks
   # Loads data into the database url from the source path. Picks up .yml and .json
   # @param [String] db_url the url of the database to load the data in to
   # @param [String] source_path the path to search for .yml and .json files
-  def self.load_data_for_database(db_url, source_path)
+  def self.load_data_for_database(db_url, base_path, database)
     @db = Throne::Database.new(db_url)
     items = []
     doccount = 0
     # grab and parse all YML files
-    Dir.glob(source_path + '/**/*.yml').each do |yml|
+    paths_for_item(base_path, database, 'data/**/*.yml').each do |yml|
       items << YAML::load(File.open(yml))
     end
     # and json
-    Dir.glob(source_path + '/**/*.json').each do |json|
+    paths_for_item(base_path, database, 'data/**/*.json').each do |json|
       items << JSON.parse(File.open(json).read)
     end
     # load em up
@@ -224,13 +239,13 @@ class Throne::Tasks
       else
         puts "There is something funky with the data for #{source_path}"
       end
-      puts "#{doccount} document(s) loaded into database at #{@db.url}"
     end
+    puts "#{doccount} document(s) loaded into database at #{@db.url}"
   end
 
   def self.dump_docs_from_db(db, base_path, format)
     raise "You must specify a DB name to dump task[dbname]" unless db
-    outdir = File.join(base_path, db)
+    outdir = File.join(base_path, db, "data")
     FileUtils.mkdir_p(outdir) unless File.exists?(outdir)
     @db = Throne::Database.new(url(db))
     docs = []
@@ -242,7 +257,7 @@ class Throne::Tasks
     outfn = format == :json ? 'dump.json' : 'dump.yml'
     File.open(File.join(outdir, outfn), 'w') do |f| 
       if format == :json
-        f.puts docs.to_json
+        f.puts JSON.pretty_generate(docs)
       elsif format == :yml
         f.puts docs.to_yaml
       else
@@ -254,37 +269,50 @@ class Throne::Tasks
 
   private
 
-  def self.inject_code_includes(base_path, file)
+  def self.inject_code_includes(base_path, database, file)
     res = ''
     File.open(file).each do |line|
       if line =~ /(\/\/|#)\ ?!code (.*)/
-        inc = File.join(base_path, $2)
-        raise "Include file #{inc} does not exist" unless File.exists?(inc)
-        res += File.read(inc)
+        if File.exists?(inc = File.join(base_path, database, 'lib', $2))
+          res += File.read(inc)
+        elsif File.exists?(inc = File.join(base_path, 'lib', $2))
+          res += File.read(inc)
+        else  
+          raise "Include file #{$2} does not exist in lib/ or #{database}/lib"
+        end
       else
         res += line
       end
-      res += "\n"
     end
     res
   end
   
-  def self.dirs_in_path(path)
+  # For the gives path, returns a list of database names and their full path.
+  # exludes lib and our other dirs.
+  def self.databases_in_path(path)
     res = {}
     Dir.glob(path + '/*').each do |fn|
+      next if File.basename(fn) == 'lib'
       res[File.basename(fn)] = fn if File.directory?(fn) 
     end
     res
   end
 
-  def self.url(dbname)
-    if dburl = ENV['DB_URL']
-      return dburl
-    elsif svrurl = ENV['SERVER_URL']
+  def self.url(dbname, args=nil)
+    if svrurl = ENV['SERVER_URL']
+      return svrurl + '/' + args.db if args && args.db
       return svrurl + '/' + dbname
     else
-      raise("You must provide a SERVER_URL or DB_URL")
+      raise("You must provide a SERVER_URL")
     end
+  end
+
+  # Given the base dir, the DB name and the item path we are after, get a list
+  # of items. This includes global and DB spec.
+  # e.g for item - data , designs, lib.
+  def self.paths_for_item(base, database, item_glob)
+    items = Dir.glob(File.join(base, item_glob))
+    items.concat(Dir.glob(File.join(base, database, item_glob)))
   end
 
 end
