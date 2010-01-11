7e69dee487076cd60474076c3c59f88308a48c43
Ben Schwarz <ben.schwarz@gmail.com>
2010-01-11 16:46:45 +1100

    Using 127.0.0.1 rather than localhost for defaults. Shortened implementation of server, server=, database, database= and added base_uri. Specs to validate.

--- lib/throne.rb
+++ lib/throne.rb
@@ -11,19 +11,14 @@ module Throne
   autoload :Tasks, 'throne/tasks'
   autoload :Database, 'throne/database'
   
-  def self.server
-    @@server ||= "http://localhost:5984"
+  class << self
+    attr_accessor :server, :database
+    
+    def base_uri
+      raise Database::NameError, "no database name set" if database.nil?
+      URI.join(server, database).to_s
+    end
   end
   
-  def self.server=(address)
-    @@server = address
-  end
-  
-  def self.database
-    @@database
-  end
-  
-  def self.database=(name)
-    @@database = name
-  end
+  self.server = "http://127.0.0.1:5984"
 end
--- spec/throne_spec.rb
+++ spec/throne_spec.rb
@@ -17,17 +17,30 @@ describe Throne do
     Throne.should respond_to "database="
   end
   
-  it "should default the server to localhost:5984" do
-    Throne.server.should == "http://localhost:5984"
+  it "should default the server to 127.0.0.1:5984" do
+    Throne.server.should == "http://127.0.0.1:5984"
   end
   
   it "should set the server" do
-    Throne.server = "http://localhost:5984"
-    Throne.server.should == "http://localhost:5984"
+    Throne.server = "http://127.0.0.1:5984"
+    Throne.server.should == "http://127.0.0.1:5984"
   end
-  
+
   it "should set the database" do
     Throne.database = "appname_environment"
     Throne.database.should == "appname_environment"
+  end
+  
+  it "should respond to base_uri" do
+    Throne.should respond_to :base_uri
+  end
+  
+  it "should be a combination of the server and database strings" do
+    Throne.base_uri.should == "http://127.0.0.1:5984/appname_environment"
+  end 
+  
+  it "should raise an error without a database having been set" do
+    Throne.database = nil
+    lambda { Throne.base_uri }.should raise_error(Throne::Database::NameError)
   end
 end
