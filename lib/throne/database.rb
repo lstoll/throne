6d815d625648922232d5e061561affaa9b11eaf6
Ben Schwarz <ben.schwarz@gmail.com>
2010-01-11 19:57:02 +1100

    Throne::Database only creates and deletes databases. Specs to validate.

--- lib/throne/database.rb
+++ lib/throne/database.rb
@@ -1,139 +1,15 @@
-require 'cgi'
-
-# Represents a connection to the database. Can be used standalone to talk
-# to a couch instance manually, otherwise is used by the mapper.
-class Throne::Database
-  attr_reader :url
-
-  # Create a new instance, with the URL for the database to work with
-  # By default will create the database if it doesn't exist, pass false
-  # as the second parameter to disable this.
-  def initialize(url, autocreate = true)
-    @url = url
-    create_database if autocreate
-  end
-
-  # Creates this database, will not error if the database exists
-  def create_database
-    begin
-      C.put @url, {}
+class Throne::Database  
+  class NameError < StandardError; end
+  
+  class << self
+    def create
+      Throne::Request.put
     rescue RestClient::RequestFailed => e
-      unless e.message =~ /412$/
-        raise e
-      end
+      super unless e.message =~ /412$/
     end
-  end
-
-  # deletes this database.
-  def delete_database
-    begin
-      C.delete @url
-    rescue RestClient::ResourceNotFound
+    
+    def delete
+      Throne::Request.delete
     end
-  end
-
-  # gets a document by it's ID
-  # 
-  # @param [String] docid the ID of the document to retrieve
-  # @param [String] rev (optional) the revision of the document to retrieve
-  # @return [Hash, nil] the document mapped to a hash, or nil if not found.
-  def get(docid, rev=nil)
-    begin
-      revurl = rev ? "?rev=#{rev}" : ""
-      Hashie::Mash.new(JP.parse(C.get(@url + '/' + docid + revurl)))
-    rescue RestClient::ResourceNotFound
-      nil
-    end
-  end
-
-  # creates/updates a document from a hash/array structure
-  def save(doc)
-    if id = doc['_id']
-      res = C.put(@url + '/' + id, JE.encode(doc))
-    else
-      res = C.post(@url, JE.encode(doc))
-    end
-    res = JP.parse(res) 
-    return nil unless res['ok']
-    Throne::StringWithRevision.new(res['id'], res['rev'])
-  end
-
-  # deletes a document. Can take an object or id
-  def delete(doc)
-    if doc.kind_of? String
-      rev = get(doc)['_rev']
-    else
-      rev = doc['_rev']
-      doc = doc['_id']
-    end
-
-    C.delete(@url + '/' + doc + '?rev=' + rev)
-  end
-
-  # runs a function by path, returning an array of results.
-  def function(path, params = {})
-    items = []
-    function_iter(path, params) {|i| items << i}
-    items
-  end
-
-
-  # runs a function by path, invoking once for each item. 
-  def function_iter(path, params = {}, &block)
-    url = @url + '/' + path
-    res = JP.parse(C.get(paramify_url(url, params)))
-    res = Throne::ArrayWithFunctionMeta.new(res['rows'], res['offset'])
-    if block_given?
-      # TODO - stream properly, need to get objects.
-      res.each do |i|
-        yield Hashie::Mash.new(i)
-      end
-      nil
-    else
-      Hashie::Mash.new(res)
-    end
-  end
-
-  private
-
-  def paramify_url url, params = {}
-    if params && !params.empty?
-      query = params.collect do |k,v|
-        v = JE.encode(v) if %w{key startkey endkey}.include?(k.to_s) && 
-          (v.kind_of?(Array) || v.kind_of?(Hash))
-        "#{k}=#{CGI.escape(v.to_s)}"
-      end.join("&")
-      url = "#{url}?#{query}"
-    end
-    url
-  end
-  
-
-  C = RestClient
-  # This is so I can switch stuff later.
-  JP = Yajl::Parser
-  JE = Yajl::Encoder
-end
-
-# Extended string, to store the couch revision
-class Throne::StringWithRevision < String
-  # Couch revision ID
-  attr_reader :revision
-
-  def initialize(id, rev)
-    @revision = rev
-    super(id)
   end
 end
-
-# Extended array, to store the couch extra data
-class Throne::ArrayWithFunctionMeta < DelegateClass(Array)
-  # Offset field as returned by couch
-  attr_reader :offset
-  
-  def initialize(array, offset)
-    @offset = offset
-    super(array)
-  end
-end
-
--- spec/throne/database_spec.rb
+++ spec/throne/database_spec.rb
@@ -1,99 +1,37 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Throne::Database do
-  before :all do
-    @dburl =  'http://localhost:5984/throne_test'
-    begin
-      RestClient.delete(@dburl)
-    rescue
-    end
-    @db = Throne::Database.new(@dburl)
-    # some kind of fixtures loading?
-  end
-
-  describe "creating/deleting databases" do
-    it "should be able to delete a database" do
-      @db.create_database
-      @db.delete_database
-      lambda {RestClient.get(@dburl)}.
-        should raise_error(RestClient::ResourceNotFound)
-    end
-
-    it "should be able to create a database" do
-      @db.delete_database
-      @db.create_database
-      lambda {RestClient.get(@dburl)}.
-        should_not raise_error(RestClient::ResourceNotFound)
-    end
-
-    it "should not error when creating a database that exists" do
-      @db.delete_database
-      @db.create_database
-      lambda {@db.create_database}.
-        should_not raise_error(Exception)
-    end
-
-    it "shoudld automatically create the database" do
-      @db.delete_database
-      @db = Throne::Database.new(@dburl)
-      lambda {RestClient.get(@dburl)}.
-        should_not raise_error(RestClient::ResourceNotFound)
+  describe "with nothing set" do
+    it "should raise an error" do
+      lambda { Throne::Database.create }.should raise_error(Throne::Database::NameError)
     end
   end
-
-  describe "working with documents" do
+  
+  describe "with a database set" do
     before :all do
-      @db = Throne::Database.new(@dburl)
+      @db_name = "throne-database-set"
+      Throne.database = @db_name
+      
+      begin
+        Throne::Database.delete
+      rescue RestClient::ResourceNotFound
+      end
     end
-
-    it "should return a valid ID and rev when creating" do
-      id = @db.save(:testfield => 'true')
-      id.should_not be_empty
-      id.revision.should_not be_empty
-      lambda {RestClient.get(@dburl + '/' + id)}.
-        should_not raise_error(Exception)
+    
+    it "should create the database" do
+      Throne::Database.create
+      lambda { RestClient.get("http://127.0.0.1:5948/#{@db_name}") }.should_not raise_error(RestClient::ResourceNotFound)
     end
-
-    it "should be able to get a doc by it's ID" do
-      id = @db.save(:testfield => 'true')
-      doc = @db.get(id)
-      doc.should_not be_nil
-      doc['testfield'].should_not be_nil
+    
+    it "should destroy the database" do
+      Throne::Database.create
+      Throne::Database.delete
+      lambda { Throne::Database.delete }.should raise_error(RestClient::ResourceNotFound)
     end
-
-    it "should be able to get a doc by it's ID and rev" do
-      id = @db.save(:testfield => 'true')
-      rev = id.revision
-      doc = @db.get(id)
-      doc['testfield2'] = 'bwah'
-      id = @db.save(doc)
-      doc = @db.get(id, rev)
-      doc['_rev'].should_not eql(id.revision)
+    
+    it "should not error when creating a database that exists" do
+      Throne::Database.create
+      lambda { Throne::Database.create }.should_not raise_error
     end
-
-    it "should be able to update an existing doc" do
-      id =  @db.save(:testfield => 'true')
-      doc = @db.get(id)
-      doc['testerfield'] = 'false'
-      @db.save(doc)
-      @db.get(id)['testerfield'].should eql('false')
-    end
-
-    it "should be able to delete a document when given a document" do
-      id = @db.save(:testfield => 'true')
-      doc = @db.get(id)
-      @db.delete(doc)
-      @db.get(id).should be_nil
-    end
-
-    it "should be able to delete a document when given a document id" do
-      id = @db.save(:testfield => 'true')
-      @db.delete(id)
-      @db.get(id).should be_nil
-    end
-  end
-
-  describe "Design Access" do
-    it "should be able to call a design with parameters"
   end
 end
