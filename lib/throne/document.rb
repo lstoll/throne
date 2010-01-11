a1c263107d08626e126e85251e41ad5b9a7837d5
Ben Schwarz <ben.schwarz@gmail.com>
2010-01-11 19:58:25 +1100

    Specced out how Throne::Document will function. Implementation pending.

--- lib/throne/document.rb
+++ lib/throne/document.rb
@@ -1,4 +1,98 @@
-# Represents a Document. Fields are defined here, as well as callbacks and
-# view access:
-class Throne::Document
+require 'cgi'
+
+class Throne::Document < Hashie::Mash
+  class ImmutableProperty < StandardError; end
+  
+  class << self
+    # Create a new document and persist it to the database
+    def create(properties = {})
+      new(properties).save
+    end
+    
+    # Get a document from the database
+    # Remove a document from the database
+  end
+    
+  # Create a new document
+
+  # Get a document from the database
+  # @param [String] docid the ID of the document to retrieve
+  # @param [String] rev (optional) the revision of the document to retrieve
+  # @return [Hash, nil] the document mapped to a hash, or nil if not found.
+  def get(docid, rev=nil)
+    begin
+      revurl = rev ? "?rev=#{rev}" : ""
+      Hashie::Mash.new(JP.parse(C.get(@url + '/' + docid + revurl)))
+    rescue RestClient::ResourceNotFound
+      nil
+    end
+  end
+
+  # Persist a document to the database
+  # @param [Hash] The document properties
+  # @return [self]
+  def save(doc = self.to_hash)
+    if doc["_id"]
+      res = Throne::Database.put _id, doc
+    else
+      res = Throne::Database.post nil, doc
+    end
+    res = JP.parse(res) 
+    return nil unless res['ok']
+    Throne::StringWithRevision.new(res['id'], res['rev'])
+  end
+
+  # deletes a document. Can take an object or id
+  def delete(doc)
+    if doc.kind_of? String
+      rev = get(doc)['_rev']
+    else
+      rev = doc['_rev']
+      doc = doc['_id']
+    end
+
+    C.delete(@url + '/' + doc + '?rev=' + rev)
+  end
+
+  # runs a function by path, returning an array of results.
+  def function(path, params = {})
+    items = []
+    function_iter(path, params) {|i| items << i}
+    items
+  end
+
+
+  # runs a function by path, invoking once for each item. 
+  def function_iter(path, params = {}, &block)
+    url = @url + '/' + path
+    res = JP.parse(C.get(paramify_url(url, params)))
+    res = Throne::ArrayWithFunctionMeta.new(res['rows'], res['offset'])
+    if block_given?
+      # TODO - stream properly, need to get objects.
+      res.each do |i|
+        yield Hashie::Mash.new(i)
+      end
+      nil
+    else
+      Hashie::Mash.new(res)
+    end
+  end
+
+  private
+
+  def paramify_url url, params = {}
+    if params && !params.empty?
+      query = params.collect do |k,v|
+        v = JE.encode(v) if %w{key startkey endkey}.include?(k.to_s) && 
+          (v.kind_of?(Array) || v.kind_of?(Hash))
+        "#{k}=#{CGI.escape(v.to_s)}"
+      end.join("&")
+      url = "#{url}?#{query}"
+    end
+    url
+  end
+  
+  # This is so I can switch stuff later.
+  JP = Yajl::Parser
+  JE = Yajl::Encoder
 end
--- spec/throne/document_spec.rb
+++ spec/throne/document_spec.rb
@@ -1,6 +1,119 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Throne::Document do
+  before :all do
+    Throne.database = "throne-document-specs"
+    Throne::Database.create
+  end
+
+  describe "crud" do
+    describe "with class methods" do
+      before :all do
+        @doc = Throne::Document.create(:field => true)
+      end
+      
+      it "should create a document" do
+        @doc.should be_an_instance_of(Throne::Document)
+        lambda { RestClient.get(URI.join(Throne.base_uri, @doc.id)) }.should_not raise_error
+      end
+
+      it "should get a document" do
+        Throne::Document.get(@doc.id).should == @doc
+      end
+      
+      it "should get a revision of a document" do
+        Throne::Document.get(@doc.id).should == @doc
+      end
+
+      it "should delete a document" do
+        doc = Throne::Document.create(:field => true)
+        Throne::Document.delete(doc.id).should be_true
+        lambda { Throne::Document.get(doc.id) }.should raise_error(Throne::Document::NotFound)
+      end      
+    end
+    
+    describe "with instance methods" do
+      before :each do
+        @doc = Throne::Document.new(:field => true)
+      end
+      
+      it "should save the document" do
+        @doc.save.should be_an_instance_of(Throne::Document)
+      end
+
+      it "should delete the document" do
+        @doc.delete.should be_true
+        lambda { Throne::Document.get(@doc.id) }.should raise_error(Throne::Document::NotFound)
+      end
+    end
+  end
+  
+  describe "instance" do
+    before :each do
+      @doc = Throne::Document.create(:field => true)
+    end
+    
+    it "should be a new_document" do
+      Throne::Document.new(:field => true).should be_new_document
+    end
+    
+    it "should not be a new_document" do
+      Throne::Document.create(:field => true).should_not be_new_document
+    end
+    
+    it "should have an id" do
+      @doc.id.should_not be_nil
+    end
+    
+    it "should have an _id" do
+      @doc._id.should_not be_nil
+    end
+    
+    it "id and _id should be the same" do
+      @doc.id.should == @doc._id
+    end
+    
+    it "should have a revision" do
+      @doc.revision.should_not be_nil
+    end
+    
+    it "should have a _rev" do
+      @doc._rev.should_not be_nil
+    end
+    
+    it "revision and _rev should be the same" do
+      @doc.revision.should == @doc._rev
+    end
+    
+    it "should be able to update an existing document" do
+      @doc.field = false
+      @doc.save
+      @doc.reload!
+      @doc.field.should be_false
+    end
+    
+    %w(id _id revision _rev).each do |m|
+      it "should not allow changes to #{m}" do
+        lambda { doc.send("#{m}=", "abc") }.should raise_error(Throne::Document::ImmutableProperty)
+      end
+    end
+    
+    describe "comparison" do
+      it "should be the same" do
+        @doc.should == @doc
+      end
+      
+      it "should not be the same" do
+        @doc.should != Throne::Document.create(:field => true)
+      end
+    end
+  end
+ 
+  describe "Design Access" do
+    it "should be able to call a design with parameters"
+  end
+    
+    
   it "should save when created called"
   it "should tag its class when saved"
   describe "View Access" do
