189920c1ce9a9cb0d7c785f258425749e8b900c6
Ben Schwarz <ben.schwarz@gmail.com>
2010-01-11 12:39:49 +1100

    Moved specs into spec/throne to follow module structure of lib/

--- spec/throne/document_spec.rb
+++ spec/throne/document_spec.rb
@@ -1,4 +1,4 @@
-require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Throne::Document do
   it "should save when created called"
--- spec/throne/tasks_spec.rb
+++ spec/throne/tasks_spec.rb
@@ -1,4 +1,4 @@
-require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
 describe Throne::Tasks do
   it "should be able to create a database"
