a992529568da7b9db13e4af8338b51a1f128f8c3
Ben Schwarz <ben.schwarz@gmail.com>
2010-01-11 19:57:50 +1100

    Autoloading request

--- lib/throne.rb
+++ lib/throne.rb
@@ -2,13 +2,10 @@ require 'yajl'
 require 'rest_client'
 require 'hashie'
 
-$:.unshift File.dirname(__FILE__) unless
-  $:.include?(File.dirname(__FILE__)) ||
-  $:.include?(File.expand_path(File.dirname(__FILE__)))
-
 module Throne
+  autoload :Tasks,    'throne/tasks'
+  autoload :Request,  'throne/request'
   autoload :Document, 'throne/document'
-  autoload :Tasks, 'throne/tasks'
   autoload :Database, 'throne/database'
   
   class << self
