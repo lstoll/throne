dfe2b5d164024a5ada00eb005a0f2187c32176c2
Ben Schwarz <ben.schwarz@gmail.com>
2010-01-12 20:59:51 +1100

    Use an instance of a class that inherits from Throne::Document

--- spec/throne/document_spec.rb
+++ spec/throne/document_spec.rb
@@ -1,5 +1,9 @@
 require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
 
+class TestDocument < Throne::Document
+  property :field
+end
+
 describe Throne::Document do
   before :all do
     Throne.database = "throne-document-specs"
@@ -9,7 +13,7 @@ describe Throne::Document do
   describe "crud" do
     describe "with class methods" do
       before :all do
-        @doc = Throne::Document.create(:field => true)
+        @doc = TestDocument.create(:field => true)
       end
       
       it "should create a document" do
@@ -18,47 +22,47 @@ describe Throne::Document do
       end
 
       it "should get a document" do
-        Throne::Document.get(@doc.id).should == @doc
+        TestDocument.get(@doc.id).should == @doc
       end
       
       it "should get a revision of a document" do
-        Throne::Document.get(@doc.id).should == @doc
+        TestDocument.get(@doc.id).should == @doc
       end
 
       it "should delete a document" do
-        doc = Throne::Document.create(:field => true)
-        Throne::Document.delete(doc.id).should be_true
-        lambda { Throne::Document.get(doc.id) }.should raise_error(Throne::Document::NotFound)
+        doc = TestDocument.create(:field => true)
+        TestDocument.delete(doc.id).should be_true
+        lambda { TestDocument.get(doc.id) }.should raise_error(Throne::Document::NotFound)
       end      
     end
     
     describe "with instance methods" do
       before :each do
-        @doc = Throne::Document.new(:field => true)
+        @doc = TestDocument.new(:field => true)
       end
       
       it "should save the document" do
-        @doc.save.should be_an_instance_of(Throne::Document)
+        @doc.save.should be_an_instance_of(TestDocument)
       end
 
       it "should delete the document" do
         @doc.delete.should be_true
-        lambda { Throne::Document.get(@doc.id) }.should raise_error(Throne::Document::NotFound)
+        lambda { TestDocument.get(@doc.id) }.should raise_error(Throne::Document::NotFound)
       end
     end
   end
   
   describe "instance" do
     before :each do
-      @doc = Throne::Document.create(:field => true)
+      @doc = TestDocument.create(:field => true)
     end
     
     it "should be a new_document" do
-      Throne::Document.new(:field => true).should be_new_document
+      TestDocument.new(:field => true).new_record?.should be_true
     end
     
     it "should not be a new_document" do
-      Throne::Document.create(:field => true).should_not be_new_document
+      TestDocument.create(:field => true).new_record?.should be_false
     end
     
     it "should have an id" do
@@ -92,21 +96,20 @@ describe Throne::Document do
       @doc.field.should be_false
     end
     
-    %w(id _id revision _rev).each do |m|
-      it "should not allow changes to #{m}" do
-        lambda { doc.send("#{m}=", "abc") }.should raise_error(Throne::Document::ImmutableProperty)
-      end
-    end
-    
     describe "comparison" do
       it "should be the same" do
         @doc.should == @doc
       end
       
       it "should not be the same" do
-        @doc.should != Throne::Document.create(:field => true)
+        @doc.should != TestDocument.create(:field => true)
       end
     end
+  end
+  
+  describe "permalinking" do
+    it "id and permalink should be the same"
+    it "should save to the permalink url"
   end
  
   describe "Design Access" do
