9ee6db384c97c82d669b3f5b3e630d59ba677dc3
Ben Schwarz <ben.schwarz@gmail.com>
2010-01-12 20:57:21 +1100

    Added json parsing of returned results. Specs to validate

--- lib/throne/request.rb
+++ lib/throne/request.rb
@@ -1,19 +1,20 @@
+require 'cgi'
 class Throne::Request
-  class << self
-    def get(resource = '', params = {})
-      RestClient.get(build_uri(resource, params), options)
+  class << self    
+    def get(request = {})
+      Yajl::Parser.parse(RestClient.get(build_uri(request.delete(:resource), request), options))
     end
 
-    def delete(resource = '', params = {})
-      RestClient.delete(build_uri(resource, params), options)
+    def delete(request = {})
+      Yajl::Parser.parse(RestClient.delete(build_uri(request.delete(:resource), request), options))
     end
 
-    def put(resource = '', data = {})
-      RestClient.put(build_uri(resource), data, options)
+    def put(request = {})
+      Yajl::Parser.parse(RestClient.put(build_uri(request.delete(:resource), request.delete(:params)), Yajl::Encoder.encode(request), options))
     end
 
-    def post(resource = '', data = {})
-      RestClient.post(build_uri(resource), data, options)
+    def post(request = {})
+      Yajl::Parser.parse(RestClient.post(build_uri(request.delete(:resource), request.delete(:params)), Yajl::Encoder.encode(request), options))
     end
 
     private
@@ -21,16 +22,17 @@ class Throne::Request
       { :accept_encoding  => "gzip, deflate" }
     end
 
-    def build_uri(resource = '', params = nil)
-      URI.join(Throne.base_uri, resource, paramify(params)).to_s
+    def build_uri(resource, params)
+      raise Throne::Database::NameError, "no database name set" if Throne.database.nil?
+      URI.join(Throne.server, Throne.database, (resource||''), paramify(params)).to_s
     end
 
-    def paramify(params)
+    def paramify(params = {})
       if params && !params.empty?
         query = params.map do |k,v|
           v = Yajl::Encoder.encode(v) if %w{key startkey endkey}.include?(k.to_s) && (v.kind_of?(Array) || v.kind_of?(Hash))
 
-          "#{k}=#{URI.escape(v.to_s)}"
+          "#{k}=#{CGI.escape(v.to_s)}"
         end
 
         "?" + query.join("&")
--- spec/throne/request_spec.rb
+++ spec/throne/request_spec.rb
@@ -5,13 +5,67 @@ describe Throne::Document do
     Throne.database = "throne-request-specs"
   end
   
+  it "should raise an error without a database having been set" do
+    Throne.database = nil
+    lambda { Throne::Request.get }.should raise_error(Throne::Database::NameError)
+    Throne.database = "throne-request-specs"
+  end
+  
   it "should make all requests asking for gzip and deflate" do
-    RestClient.should_receive(:put).with("http://127.0.0.1:5984/throne-request-specs", {}, { :accept_encoding  => "gzip, deflate" })
+    RestClient.should_receive(:put).with("http://127.0.0.1:5984/throne-request-specs", "{}", { :accept_encoding  => "gzip, deflate" })
     Throne::Database.create
   end
   
-  describe "get"  
-  describe "delete"  
-  describe "put"
-  describe "post"
+  shared_examples_for "paramification" do    
+    it "should be json encoded" do
+      RestClient.should_receive(:get).with("http://127.0.0.1:5984/document?startkey=%5B%22abc%22%2C%22xyz%22%5D", {:accept_encoding=>"gzip, deflate"})
+      Throne::Request.get :resource => "document", :params => {:startkey => ["abc", "xyz"]}
+    end
+    
+    it "should uri escape each param value" do
+      RestClient.should_receive(:get).with("http://127.0.0.1:5984/document?descending=true", {:accept_encoding=>"gzip, deflate"})
+      Throne::Request.get :resource => "document", :params => {:descending => true}
+    end
+    
+    it "should join using an &" do
+      RestClient.should_receive(:get).with("http://127.0.0.1:5984/document?descending=true&limit=1", {:accept_encoding=>"gzip, deflate"})
+      Throne::Request.get :resource => "document", :params => {:descending => true, :limit => 1}
+    end
+  end
+  
+  describe "get" do
+    describe "expectations" do
+      it_should_behave_like "paramification"
+    end
+    
+    it "should get" do
+      RestClient.should_receive(:get).with("http://127.0.0.1:5984/document", {:accept_encoding=>"gzip, deflate"})
+      Throne::Request.get(:resource => "document").should be_an_instance_of(Hash)
+    end
+  end
+  
+  describe "delete" do
+    describe "expectations" do
+      it_should_behave_like "paramification"
+    end
+    
+    it "should delete" do
+      RestClient.should_receive(:delete).with("http://127.0.0.1:5984/document", {:accept_encoding=>"gzip, deflate"})
+      Throne::Request.delete(:resource => "document").should be_an_instance_of(Hash)
+    end
+  end
+  
+  describe "put" do
+    it "should put" do
+      RestClient.should_receive(:put).with("http://127.0.0.1:5984/document", "{}", {:accept_encoding=>"gzip, deflate"})
+      Throne::Request.put(:resource => "document").should be_an_instance_of(Hash)
+    end
+  end
+  
+  describe "post" do
+    it "should post" do
+      RestClient.should_receive(:post).with("http://127.0.0.1:5984/document", "{}", {:accept_encoding=>"gzip, deflate"})
+      Throne::Request.post(:resource => "document").should be_an_instance_of(Hash)
+    end
+  end
 end
