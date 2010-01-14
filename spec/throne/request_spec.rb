6921c22e32728a60cf89e5d166a55891b04c8a8e
Ben Schwarz <ben.schwarz@gmail.com>
2010-01-14 17:56:20 +1100

    Request spec matches the new Array join used to create the url

--- spec/throne/request_spec.rb
+++ spec/throne/request_spec.rb
@@ -12,23 +12,23 @@ describe Throne::Document do
   end
   
   it "should make all requests asking for gzip and deflate" do
-    RestClient.should_receive(:put).with("http://127.0.0.1:5984/throne-request-specs", "{}", { :accept_encoding  => "gzip, deflate" })
+    RestClient.should_receive(:put).with("http://127.0.0.1:5984/throne-request-specs/", "{}", { :accept_encoding  => "gzip, deflate" })
     Throne::Database.create
   end
   
   shared_examples_for "paramification" do    
     it "should be json encoded" do
-      RestClient.should_receive(:get).with("http://127.0.0.1:5984/document?startkey=%5B%22abc%22%2C%22xyz%22%5D", {:accept_encoding=>"gzip, deflate"})
+      RestClient.should_receive(:get).with("http://127.0.0.1:5984/throne-request-specs/document?startkey=%5B%22abc%22%2C%22xyz%22%5D", {:accept_encoding=>"gzip, deflate"})
       Throne::Request.get :resource => "document", :params => {:startkey => ["abc", "xyz"]}
     end
     
     it "should uri escape each param value" do
-      RestClient.should_receive(:get).with("http://127.0.0.1:5984/document?descending=true", {:accept_encoding=>"gzip, deflate"})
+      RestClient.should_receive(:get).with("http://127.0.0.1:5984/throne-request-specs/document?descending=true", {:accept_encoding=>"gzip, deflate"})
       Throne::Request.get :resource => "document", :params => {:descending => true}
     end
     
     it "should join using an &" do
-      RestClient.should_receive(:get).with("http://127.0.0.1:5984/document?descending=true&limit=1", {:accept_encoding=>"gzip, deflate"})
+      RestClient.should_receive(:get).with("http://127.0.0.1:5984/throne-request-specs/document?descending=true&limit=1", {:accept_encoding=>"gzip, deflate"})
       Throne::Request.get :resource => "document", :params => {:descending => true, :limit => 1}
     end
   end
@@ -39,7 +39,7 @@ describe Throne::Document do
     end
     
     it "should get" do
-      RestClient.should_receive(:get).with("http://127.0.0.1:5984/document", {:accept_encoding=>"gzip, deflate"})
+      RestClient.should_receive(:get).with("http://127.0.0.1:5984/throne-request-specs/document", {:accept_encoding=>"gzip, deflate"})
       Throne::Request.get(:resource => "document")
     end
   end
@@ -50,21 +50,21 @@ describe Throne::Document do
     end
     
     it "should delete" do
-      RestClient.should_receive(:delete).with("http://127.0.0.1:5984/document", {:accept_encoding=>"gzip, deflate"})
+      RestClient.should_receive(:delete).with("http://127.0.0.1:5984/throne-request-specs/document", {:accept_encoding=>"gzip, deflate"})
       Throne::Request.delete(:resource => "document")
     end
   end
   
   describe "put" do
     it "should put" do
-      RestClient.should_receive(:put).with("http://127.0.0.1:5984/document", "{}", {:accept_encoding=>"gzip, deflate"})
+      RestClient.should_receive(:put).with("http://127.0.0.1:5984/throne-request-specs/document", "{}", {:accept_encoding=>"gzip, deflate"})
       Throne::Request.put(:resource => "document")
     end
   end
   
   describe "post" do
     it "should post" do
-      RestClient.should_receive(:post).with("http://127.0.0.1:5984/document", "{}", {:accept_encoding=>"gzip, deflate"})
+      RestClient.should_receive(:post).with("http://127.0.0.1:5984/throne-request-specs/document", "{}", {:accept_encoding=>"gzip, deflate"})
       Throne::Request.post(:resource => "document")
     end
   end
